a47aa11457e7494189d7ef57b6a93def50010529
dinhthao92 <dinhthao92@gmail.com>
2025-06-21 18:57:36 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/hpdq-acs/", // ✅ Thêm dòng này
   server: {
     host: "::",
     port: 8080,
